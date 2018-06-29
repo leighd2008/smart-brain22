22f58c1ba984e456ef6cca1cda434028202a535e
Diane Leigh <leighd2008@gmail.com>
2018-06-29 19:48:22 -0400

    initial coding for using face recognition api

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: 'bec6809596454b12b1a19fc5be249c46'
+});
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log('check')
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+      .then(
+      function(response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+// Clarifai.FACE_DETECT_MODEL
+//
+// app.models
+// .predict(
+// Clarifai.COLOR_MODEL,
+//     // URL
+//     "https://samples.clarifai.com/metro-north.jpg"
+// )
+// .then(function(response) {
+//     // do something with responseconsole.log(response);
+//     },
+//     function(err) {// there was an error}
+// );
